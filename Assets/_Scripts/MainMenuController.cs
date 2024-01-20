36f53f2641322376044af1f71c301d455d7f45b7
Chit Moe Wai <chitmoewai135@gmail.com>
2024-01-20 18:57:46 +0630

    chitmoe : chartcter set up in progress

--- Assets/_Scripts/MainMenuController.cs
+++ Assets/_Scripts/MainMenuController.cs
@@ -6,7 +6,7 @@ using Photon.Realtime;
 
 public class MainMenuController : MonoBehaviourPunCallbacks
 {
-    private int MaxPlayersPerRoom = 2;
+    private int MaxPlayersPerRoom = 1;
 
     [SerializeField] private GameObject ConnectPanel;
     [SerializeField] private GameObject WaitingRoomPanel;
